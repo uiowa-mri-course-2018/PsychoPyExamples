a989b2a59c6bc83e04674088d790ca0973746fb8
James Kent <james-kent@uiowa.edu>
2018-01-29 11:17:39 -0600

    first_commit

---
+++ event/event_design_lastrun.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+This experiment was created using PsychoPy2 Experiment Builder (v1.85.3),
+    on Thu 25 Jan 2018 05:07:10 PM CST
+If you publish work using this script please cite the PsychoPy publications:
+    Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
+        Journal of Neuroscience Methods, 162(1-2), 8-13.
+    Peirce, JW (2009) Generating stimuli for neuroscience using PsychoPy.
+        Frontiers in Neuroinformatics, 2:10. doi: 10.3389/neuro.11.010.2008
+"""
+
+from __future__ import absolute_import, division
+from psychopy import locale_setup, sound, gui, visual, core, data, event, logging, hardware
+from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
+                                STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
+import numpy as np  # whole numpy lib is available, prepend 'np.'
+from numpy import (sin, cos, tan, log, log10, pi, average,
+                   sqrt, std, deg2rad, rad2deg, linspace, asarray)
+from numpy.random import random, randint, normal, shuffle
+import os  # handy system and path functions
+import sys  # to get file system encoding
+
+# Ensure that relative paths start from the same directory as this script
+_thisDir = os.path.dirname(os.path.abspath(__file__)).decode(sys.getfilesystemencoding())
+os.chdir(_thisDir)
+
+# Store info about the experiment session
+expName = 'event_design'  # from the Builder filename that created this script
+expInfo = {'participant':'', 'session':'001'}
+dlg = gui.DlgFromDict(dictionary=expInfo, title=expName)
+if dlg.OK == False:
+    core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+
+# Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
+filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expName, expInfo['date'])
+
+# An ExperimentHandler isn't essential but helps with data saving
+thisExp = data.ExperimentHandler(name=expName, version='',
+    extraInfo=expInfo, runtimeInfo=None,
+    originPath=u'/home/james/event_design.psyexp',
+    savePickle=True, saveWideText=True,
+    dataFileName=filename)
+# save a log file for detail verbose info
+logFile = logging.LogFile(filename+'.log', level=logging.EXP)
+logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
+
+endExpNow = False  # flag for 'escape' or other condition => quit the exp
+
+# Start Code - component code to be run before the window creation
+
+# Setup the Window
+win = visual.Window(
+    size=(1366, 768), fullscr=True, screen=0,
+    allowGUI=False, allowStencil=False,
+    monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
+    blendMode='avg', useFBO=True)
+# store frame rate of monitor if we can measure it
+expInfo['frameRate'] = win.getActualFrameRate()
+if expInfo['frameRate'] != None:
+    frameDur = 1.0 / round(expInfo['frameRate'])
+else:
+    frameDur = 1.0 / 60.0  # could not measure, so guess
+
+# Initialize components for Routine "trial"
+trialClock = core.Clock()
+square = visual.Rect(
+    win=win, name='square',
+    width=(0.5, 0.5)[0], height=(0.5, 0.5)[1],
+    ori=0, pos=(0, 0),
+    lineWidth=1, lineColor=[1,1,1], lineColorSpace='rgb',
+    fillColor=[1,1,1], fillColorSpace='rgb',
+    opacity=1, depth=0.0, interpolate=True)
+inter_trial_interval = core.StaticPeriod(win=win, screenHz=expInfo['frameRate'], name='inter_trial_interval')
+
+# Create some handy timers
+globalClock = core.Clock()  # to track the time since experiment started
+routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
+
+# set up handler to look after randomisation of conditions etc
+trials = data.TrialHandler(nReps=2, method='random', 
+    extraInfo=expInfo, originPath=-1,
+    trialList=data.importConditions('event_rt.csv'),
+    seed=1, name='trials')
+thisExp.addLoop(trials)  # add the loop to the experiment
+thisTrial = trials.trialList[0]  # so we can initialise stimuli with some values
+# abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
+if thisTrial != None:
+    for paramName in thisTrial.keys():
+        exec(paramName + '= thisTrial.' + paramName)
+
+for thisTrial in trials:
+    currentLoop = trials
+    # abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
+    if thisTrial != None:
+        for paramName in thisTrial.keys():
+            exec(paramName + '= thisTrial.' + paramName)
+    
+    # ------Prepare to start Routine "trial"-------
+    t = 0
+    trialClock.reset()  # clock
+    frameN = -1
+    continueRoutine = True
+    # update component parameters for each repeat
+    keyboard = event.BuilderKeyResponse()
+    # keep track of which components have finished
+    trialComponents = [square, inter_trial_interval, keyboard]
+    for thisComponent in trialComponents:
+        if hasattr(thisComponent, 'status'):
+            thisComponent.status = NOT_STARTED
+    
+    # -------Start Routine "trial"-------
+    while continueRoutine:
+        # get current time
+        t = trialClock.getTime()
+        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+        # update/draw components on each frame
+        
+        # *square* updates
+        if t >= 0.0 and square.status == NOT_STARTED:
+            # keep track of start time/frame for later
+            square.tStart = t
+            square.frameNStart = frameN  # exact frame index
+            square.setAutoDraw(True)
+        frameRemains = 0.0 + 1.0- win.monitorFramePeriod * 0.75  # most of one frame period left
+        if square.status == STARTED and t >= frameRemains:
+            square.setAutoDraw(False)
+        
+        # *keyboard* updates
+        if t >= 0.0 and keyboard.status == NOT_STARTED:
+            # keep track of start time/frame for later
+            keyboard.tStart = t
+            keyboard.frameNStart = frameN  # exact frame index
+            keyboard.status = STARTED
+            # keyboard checking is just starting
+            win.callOnFlip(keyboard.clock.reset)  # t=0 on next screen flip
+            event.clearEvents(eventType='keyboard')
+        frameRemains = 0.0 + 1.0- win.monitorFramePeriod * 0.75  # most of one frame period left
+        if keyboard.status == STARTED and t >= frameRemains:
+            keyboard.status = STOPPED
+        if keyboard.status == STARTED:
+            theseKeys = event.getKeys(keyList=['space'])
+            
+            # check for quit:
+            if "escape" in theseKeys:
+                endExpNow = True
+            if len(theseKeys) > 0:  # at least one key was pressed
+                keyboard.keys = theseKeys[-1]  # just the last key pressed
+                keyboard.rt = keyboard.clock.getTime()
+        # *inter_trial_interval* period
+        if t >= 1 and inter_trial_interval.status == NOT_STARTED:
+            # keep track of start time/frame for later
+            inter_trial_interval.tStart = t
+            inter_trial_interval.frameNStart = frameN  # exact frame index
+            inter_trial_interval.start(iti)
+        elif inter_trial_interval.status == STARTED:  # one frame should pass before updating params and completing
+            inter_trial_interval.complete()  # finish the static period
+        
+        # check if all components have finished
+        if not continueRoutine:  # a component has requested a forced-end of Routine
+            break
+        continueRoutine = False  # will revert to True if at least one component still running
+        for thisComponent in trialComponents:
+            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                continueRoutine = True
+                break  # at least one component has not yet finished
+        
+        # check for quit (the Esc key)
+        if endExpNow or event.getKeys(keyList=["escape"]):
+            core.quit()
+        
+        # refresh the screen
+        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+            win.flip()
+    
+    # -------Ending Routine "trial"-------
+    for thisComponent in trialComponents:
+        if hasattr(thisComponent, "setAutoDraw"):
+            thisComponent.setAutoDraw(False)
+    # check responses
+    if keyboard.keys in ['', [], None]:  # No response was made
+        keyboard.keys=None
+    trials.addData('keyboard.keys',keyboard.keys)
+    if keyboard.keys != None:  # we had a response
+        trials.addData('keyboard.rt', keyboard.rt)
+    # the Routine "trial" was not non-slip safe, so reset the non-slip timer
+    routineTimer.reset()
+    thisExp.nextEntry()
+    
+# completed 2 repeats of 'trials'
+
+# these shouldn't be strictly necessary (should auto-save)
+thisExp.saveAsWideText(filename+'.csv')
+thisExp.saveAsPickle(filename)
+logging.flush()
+# make sure everything is closed down
+thisExp.abort()  # or data files will save again on exit
+win.close()
+core.quit()
